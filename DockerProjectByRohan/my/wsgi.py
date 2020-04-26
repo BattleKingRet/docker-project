8c43f242d46632a515bea577140eb30e4921aca3
BattleKingRet <54021130+BattleKingRet@users.noreply.github.com>
2020-04-26 21:19:18 +0530

    Update wsgi.py

--- DockerProjectByRohan/my/wsgi.py
+++ DockerProjectByRohan/my/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for volley project.
+WSGI config for my project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
